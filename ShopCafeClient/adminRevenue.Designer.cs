aee215d82ad4a979770f10a533c5c02803d8a558
hien.hv6193 <hien.hv6193@sinhvien.hoasen.edu.vn>
2022-10-15 18:54:03 +0700

    fix giao diện trang admin doanh thu

--- ShopCafeClient/adminRevenue.Designer.cs
+++ ShopCafeClient/adminRevenue.Designer.cs
@@ -34,24 +34,26 @@ namespace ShopCafeClient
             this.dtpCheckIn = new System.Windows.Forms.DateTimePicker();
             this.btnViewBill = new System.Windows.Forms.Button();
             this.dtpCheckOut = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhThu)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvDoanhThu
             // 
             this.dgvDoanhThu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDoanhThu.Location = new System.Drawing.Point(12, 64);
+            this.dgvDoanhThu.Location = new System.Drawing.Point(12, 105);
             this.dgvDoanhThu.Name = "dgvDoanhThu";
             this.dgvDoanhThu.RowHeadersWidth = 51;
             this.dgvDoanhThu.RowTemplate.Height = 24;
-            this.dgvDoanhThu.Size = new System.Drawing.Size(799, 442);
+            this.dgvDoanhThu.Size = new System.Drawing.Size(799, 401);
             this.dgvDoanhThu.TabIndex = 0;
             // 
             // dtpCheckIn
             // 
             this.dtpCheckIn.CalendarFont = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpCheckIn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpCheckIn.Location = new System.Drawing.Point(12, 11);
+            this.dtpCheckIn.Location = new System.Drawing.Point(12, 61);
             this.dtpCheckIn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtpCheckIn.Name = "dtpCheckIn";
             this.dtpCheckIn.Size = new System.Drawing.Size(307, 30);
@@ -60,7 +62,7 @@ namespace ShopCafeClient
             // btnViewBill
             // 
             this.btnViewBill.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
-            this.btnViewBill.Location = new System.Drawing.Point(352, 11);
+            this.btnViewBill.Location = new System.Drawing.Point(350, 61);
             this.btnViewBill.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnViewBill.Name = "btnViewBill";
             this.btnViewBill.Size = new System.Drawing.Size(123, 39);
@@ -72,17 +74,37 @@ namespace ShopCafeClient
             // 
             this.dtpCheckOut.CalendarFont = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpCheckOut.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtpCheckOut.Location = new System.Drawing.Point(501, 11);
+            this.dtpCheckOut.Location = new System.Drawing.Point(501, 61);
             this.dtpCheckOut.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtpCheckOut.Name = "dtpCheckOut";
             this.dtpCheckOut.Size = new System.Drawing.Size(310, 30);
             this.dtpCheckOut.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(113, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(360, 30);
+            this.textBox1.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(501, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(123, 39);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // adminRevenue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(843, 518);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dtpCheckOut);
             this.Controls.Add(this.btnViewBill);
             this.Controls.Add(this.dtpCheckIn);
@@ -93,6 +115,7 @@ namespace ShopCafeClient
             this.Text = "Doanh thu";
             ((System.ComponentModel.ISupportInitialize)(this.dgvDoanhThu)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -102,5 +125,7 @@ namespace ShopCafeClient
         private DateTimePicker dtpCheckIn;
         private Button btnViewBill;
         private DateTimePicker dtpCheckOut;
+        private TextBox textBox1;
+        private Button btnSearch;
     }
 }
