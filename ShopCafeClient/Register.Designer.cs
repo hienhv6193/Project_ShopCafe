8873cc86b0195d665431722c80ed33392e59e99a
quynguyen919 <quy.ndm9039@sinhvien.hoasen.edu.vn>
2022-10-21 13:17:09 +0700

    Category UI

--- ShopCafeClient/Register.Designer.cs
+++ ShopCafeClient/Register.Designer.cs
@@ -2,15 +2,15 @@
 
 namespace ShopCafeClient
 {
-    partial class Login
+    partial class Register
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -25,93 +25,78 @@ namespace ShopCafeClient
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnRegister = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.btxExit = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.txbPass = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.btnLogin = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.txbUser = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.btnRegister);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.btxExit);
             this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.btnLogin);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(196, 11);
+            this.panel1.Location = new System.Drawing.Point(61, 22);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(518, 284);
-            this.panel1.TabIndex = 0;
+            this.panel1.Size = new System.Drawing.Size(670, 343);
+            this.panel1.TabIndex = 1;
             // 
             // btnRegister
             // 
             this.btnRegister.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegister.Location = new System.Drawing.Point(225, 226);
+            this.btnRegister.Location = new System.Drawing.Point(292, 286);
             this.btnRegister.Name = "btnRegister";
             this.btnRegister.Size = new System.Drawing.Size(101, 35);
             this.btnRegister.TabIndex = 3;
             this.btnRegister.Text = "Đăng ký";
             this.btnRegister.UseVisualStyleBackColor = true;
-            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 21F, System.Drawing.FontStyle.Bold);
             this.label3.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.label3.Location = new System.Drawing.Point(172, 26);
+            this.label3.Location = new System.Drawing.Point(257, 21);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(181, 40);
+            this.label3.Size = new System.Drawing.Size(151, 40);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Đăng nhập";
-            // 
-            // btxExit
-            // 
-            this.btxExit.BackColor = System.Drawing.SystemColors.Window;
-            this.btxExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
-            this.btxExit.Location = new System.Drawing.Point(396, 226);
-            this.btxExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btxExit.Name = "btxExit";
-            this.btxExit.Size = new System.Drawing.Size(94, 35);
-            this.btxExit.TabIndex = 5;
-            this.btxExit.Text = "Thoát";
-            this.btxExit.UseVisualStyleBackColor = false;
-            this.btxExit.Click += new System.EventHandler(this.btxExit_Click);
+            this.label3.Text = "Đăng Ký";
             // 
             // panel3
             // 
             this.panel3.Controls.Add(this.txbPass);
             this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(16, 150);
+            this.panel3.Location = new System.Drawing.Point(71, 151);
             this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(491, 51);
+            this.panel3.Size = new System.Drawing.Size(547, 51);
             this.panel3.TabIndex = 1;
             // 
             // txbPass
             // 
             this.txbPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txbPass.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbPass.Location = new System.Drawing.Point(186, 10);
+            this.txbPass.Location = new System.Drawing.Point(221, 10);
             this.txbPass.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txbPass.Name = "txbPass";
             this.txbPass.Size = new System.Drawing.Size(288, 30);
@@ -128,34 +113,21 @@ namespace ShopCafeClient
             this.label2.TabIndex = 0;
             this.label2.Text = "Mật khẩu:";
             // 
-            // btnLogin
-            // 
-            this.btnLogin.BackColor = System.Drawing.SystemColors.Window;
-            this.btnLogin.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(30, 226);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(124, 35);
-            this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Đăng nhập";
-            this.btnLogin.UseVisualStyleBackColor = false;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
-            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.txbUser);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(16, 80);
+            this.panel2.Location = new System.Drawing.Point(71, 83);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(491, 51);
+            this.panel2.Size = new System.Drawing.Size(547, 51);
             this.panel2.TabIndex = 0;
             // 
             // txbUser
             // 
             this.txbUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txbUser.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbUser.Location = new System.Drawing.Point(186, 10);
+            this.txbUser.Location = new System.Drawing.Point(221, 11);
             this.txbUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txbUser.Name = "txbUser";
             this.txbUser.Size = new System.Drawing.Size(288, 30);
@@ -171,53 +143,70 @@ namespace ShopCafeClient
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên đăng nhập:";
             // 
-            // pictureBox1
+            // panel4
             // 
-            this.pictureBox1.Image = global::ShopCafeClient.Properties.Resources.cafe;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(178, 284);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(71, 222);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(547, 49);
+            this.panel4.TabIndex = 5;
             // 
-            // Login
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(204, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Nhập lại mật khẩu:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(221, 7);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 30);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(726, 317);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(800, 386);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Login";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Đăng nhập";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Login_FormClosing);
+            this.Name = "Register";
+            this.Text = "Đăng kí";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
+
         #endregion
 
         private Panel panel1;
+        private Button btnRegister;
+        private Label label3;
         private Panel panel3;
         private TextBox txbPass;
         private Label label2;
         private Panel panel2;
-        private Label label1;
-        private Button btxExit;
-        private Button btnLogin;
-        private Label label3;
-        private PictureBox pictureBox1;
         private TextBox txbUser;
-        private Button btnRegister;
+        private Label label1;
+        private Panel panel4;
+        private TextBox textBox1;
+        private Label label4;
     }
 }
--- ShopCafeClient/adminCategory.Designer.cs
+++ ShopCafeClient/adminCategory.Designer.cs
@@ -28,20 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.Btn_Update = new System.Windows.Forms.Button();
+            this.Btn_Delete = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.txt_Name = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(273, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh Mục";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(499, 310);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Add.Location = new System.Drawing.Point(542, 352);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Update
+            // 
+            this.Btn_Update.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Update.Location = new System.Drawing.Point(623, 352);
+            this.Btn_Update.Name = "Btn_Update";
+            this.Btn_Update.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Update.TabIndex = 3;
+            this.Btn_Update.Text = "Update";
+            this.Btn_Update.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Delete
+            // 
+            this.Btn_Delete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Delete.Location = new System.Drawing.Point(704, 352);
+            this.Btn_Delete.Name = "Btn_Delete";
+            this.Btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Delete.TabIndex = 4;
+            this.Btn_Delete.Text = "Delete";
+            this.Btn_Delete.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(555, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ID:";
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txt_ID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_ID.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt_ID.Location = new System.Drawing.Point(623, 140);
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.Size = new System.Drawing.Size(156, 22);
+            this.txt_ID.TabIndex = 7;
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.AutoSize = true;
+            this.txt_Name.Location = new System.Drawing.Point(555, 190);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(47, 16);
+            this.txt_Name.TabIndex = 8;
+            this.txt_Name.Text = "Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(623, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 22);
+            this.textBox1.TabIndex = 9;
             // 
             // adminCategory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.txt_ID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Btn_Delete);
+            this.Controls.Add(this.Btn_Update);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
             this.Name = "adminCategory";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "adminCategory";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button Btn_Update;
+        private System.Windows.Forms.Button Btn_Delete;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.Label txt_Name;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
